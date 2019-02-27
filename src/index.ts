2d80a1a8442cfda5b95ea6dda43012bf17ec7e9f
Spencer Stolworthy <sdstolworthy@gmail.com>
2019-02-27 11:51:10 -0700

    fixed cors

--- src/index.ts
+++ src/index.ts
@@ -28,6 +28,13 @@ app.use(bodyParser.json());
 app.use(cors({
   origin: false
 }));
+app.use(function(req, res: any, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 // sendSMS(process.env.TRIAL_PHONE, 'the server is running')
 
