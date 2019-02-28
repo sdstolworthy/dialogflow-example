5c6410e304dec0d773545b542c47542763f4a559
Spencer Stolworthy <sdstolworthy@gmail.com>
2019-02-27 17:59:29 -0700

    fixes

--- src/index.ts
+++ src/index.ts
@@ -152,7 +152,7 @@ const dataParsingOperations: { [key: string]: (value: any) => string } = {
     return resp;
   },
   time: (time) => {
-    const resp = moment(time).format('h:mm a');
+    const resp = moment(time).local(true).format('h:mm a');
     console.log('time', time, resp)
     return resp
   },
