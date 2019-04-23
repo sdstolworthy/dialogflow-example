ae42c11a36ac99939de3e9caa2ae5c51a18360b0
Spencer Stolworthy <sdstolworthy@gmail.com>
2019-04-23 10:55:24 -0600

    test

--- src/index.ts
+++ src/index.ts
@@ -100,11 +100,7 @@ app.get("/messages", async (req, res) => {
   const indexedMessages = {}
   Object.keys(
     messages.reduce((prev, curr) => {
-      const pn = new PhoneNumber(curr.patronPhone)
-        .getNumber("national")
-        .split("")
-        .splice(2, 4, ...new Array(4).fill("*"))
-        .join("")
+      const pn = new PhoneNumber(curr.patronPhone).getNumber("national")
       console.log(pn)
       prev[pn] = []
       return prev
